4176061b89e426df6afb359d76df6c75c7407e16
Kateryna <katiaherascevich@gmail.com>
2025-08-26 10:21:00 +0200

    Solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -6,7 +6,15 @@
 function applyCustomSort() {
   [].__proto__.sort2 = function (compareFunction) {
     const defaultComparator = (a, b) => {
-      return String(a) > String(b) ? 1 : String(a) < String(b) ? -1 : 0;
+      if (a === undefined && b === undefined) {
+        return 0;
+      } else if (a === undefined) {
+        return 1;
+      } else if (b === undefined) {
+        return -1;
+      } else {
+        return String(a) > String(b) ? 1 : String(a) < String(b) ? -1 : 0;
+      }
     };
 
     const cmp =
@@ -16,7 +24,12 @@ function applyCustomSort() {
 
     for (let i = 0; i < this.length; i++) {
       for (let j = 0; j < this.length - i - 1; j++) {
-        if (cmp(this[j], this[j + 1]) > 0) {
+        const hasA = Object.prototype.hasOwnProperty.call(this, j);
+        const hasB = Object.prototype.hasOwnProperty.call(this, j + 1);
+        const a = hasA ? this[j] : undefined;
+        const b = hasB ? this[j + 1] : undefined;
+
+        if (cmp(a, b) > 0) {
           const temp = this[j];
 
           this[j] = this[j + 1];
